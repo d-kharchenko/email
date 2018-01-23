f4cc44dd602839f41308ffa0fc1b9b4f007b4919
Dmytro Kharchenko <Kharchenko_D@island-ukraine.com>
2018-01-23 15:35:44 +0200

    test

---
+++ js/app.js
@@ -0,0 +1,1 @@
+$(function(){$(".nav-hover-link").hover(function(){$(" > *",this).toggleClass("link-arrow-active")}),$("#callback-form").submit(function(){return $.ajax({type:"POST",url:"mail.php",data:$(this).serialize()}).done(function(){$(this).find("input").val(""),alert("Спасибо за заявку! Скоро мы с вами свяжемся."),$("#form").trigger("reset")}),!1})});
